0ba3fd75622363d3df0a9c82e9ba740407e15a05
Zachary Murray <89foxlx@gmail.com>
2019-10-04 19:51:35 -0500

    added one event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+
+
+const navElementSelector = document.querySelectorAll(".main-navigation .container .nav a");
+const paragraphElementSelector = document.querySelector
+
+
+//navElementSelector.addEventListener('click', highlight(navElementSelector));
+
+for(let a = 0; a < navElementSelector.length; a++){
+    navElementSelector[a].addEventListener('click', (e) => {
+        if(e.target.style.backgroundColor != 'blue'){
+            e.target.style.backgroundColor = 'blue';
+        } else {
+            e.target.style.backgroundColor = 'white';
+        }
+        
+    });
+}
+
+
+
+// function highlight(test) {
+//     //console.log('test');
+//     test.style.backgroundColor = 'blue';
+// }
+
+
+
